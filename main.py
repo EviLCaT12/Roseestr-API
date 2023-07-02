8e0556ad24fde31cbc9772451746c36b0a7743eb
Timofey Kuznetsov <kuznetsov.timofey.2003@mail.ru>
2023-07-02 16:31:00 +1000

    added desscription for endpoint

--- main.py
+++ main.py
@@ -8,9 +8,18 @@ from typing import List
 app = FastAPI()
 
 
-# examples
 @app.post("/place/", response_model=List[Place])
 async def place(points: Points = Points(x=43.165029, y=131.242545)):
+    """
+    Параметры: \\
+    address - Адрес \\
+    cad_cost - Кадастровая стоимость(в рублях) \\
+    util_by_doc - Разрешённое использования \\
+    cc_date_entering - Дата внесения сведений \\
+    date_cost - Дата определения \\
+    application_date - Дата применения \\
+    area_value - Площадь(кв. м) \
+    """
     places = rosreestr.get_places(points.x, points.y)
     return places
 
